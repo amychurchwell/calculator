dc6ca593035e2016e3bd2306b0dc338740c80dd2
Amy Churchwell and Cat Janowitz <amy@>
2017-10-16 13:34:36 -0700

    Create directories, index.html, scripts.js and cooking_scripts.js.

---
+++ JS/cooking_scripts.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+var tbs = parseFloat(prompt('enter tablespoons:'));
+var toCup = function(tbs) {
+  return tbs/16;
+};
+alert(tbs + " tablespoons is " + toCup(tbs) + " cup(s)!");
+
+
+
+/* Wet Measurements
+
+var wetQt = parseFloat(prompt('enter liquid quarts:'));
+var qtPint = function(wetQt) {
+  return wetQt / 2;
+}
+alert(wetQt + " liquid quarts is " + qtPint(wetQt) + " pint(s)!");
+*/
+
+/*
+var wetCup = parseFloat(prompt('enter liquid cups:'));
+var cupPint = function(wetCup) {
+  return wetCup / 2;
+}
+
+alert(wetCup + " liquid cups is " + cupPint(wetCup) + " pint(s)!");
+*/
---
+++ JS/scripts.js
@@ -0,0 +1,25 @@
+/*
+var height = parseInt(prompt("What is your height in inches?"));
+var weight = parseInt(prompt("What is your weight in pounds?"));
+
+var height = height * 0.025;
+
+
+var weight = weight * 0.45;
+
+
+var sqHeight = height * height;
+
+
+var output = function(weight, sqHeight){
+  return weight / sqHeight;
+}
+
+alert("Your BMI is " + output(weight, sqHeight) + "!");
+*/
+
+var celsius = parseInt(prompt('What is the temperature in Celsius?'));
+
+var farenheit = celsius * 1.8 + 32;
+
+alert('It is ' + celsius + ' degrees Celsius. It is ' + farenheit + ' degrees Farenheit.');
